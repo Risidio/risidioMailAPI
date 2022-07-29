cd4d65f1e695f6cdcb9ac49490d32faf6b890e1b
Apower11 <59498034+Apower11@users.noreply.github.com>
2022-07-29 20:53:23 +0100

    version 1

---
+++ dist/main.js
@@ -0,0 +1,106 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require('dotenv').config();
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const cors = require('cors');
+app.use(cors());
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport({
+    pool: true,
+    host: "uk53.siteground.eu",
+    secureConnection: false,
+    port: 465,
+    tls: {
+        ciphers: 'SSLv3'
+    },
+    auth: {
+        user: process.env.A1,
+        pass: process.env.A2
+    }
+});
+transporter.verify((error) => {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log("Ready to Send");
+    }
+});
+app.post("/submit-project", (req, res) => {
+    const projectMailOptions = {
+        from: process.env.A1,
+        to: process.env.A4,
+        subject: `${req.body.name} has sent you a project idea called ${req.body.projectName}!`,
+        text: `${req.body.description}`,
+        html: `<div style="margin: 20px; text-align: left; border: solid 1px grey; border-radius: 5px; padding: 20px;">
+              <h3 style="text-align: center;">You recieved a new message from your Indige Form </h3>
+              <hr style="margin: 20px; color: grey;"/>
+              <br/>
+              <h3>Name:</h3>
+              <p>${req.body.name}</p>
+              <h3>Email:</h3>
+              <p>${req.body.email}</p>
+              <h3>Project Name:</h3>
+              <p>${req.body.projectName}</p>
+              <h3>Project Description:</h3>
+              <p>${req.body.description}</p>
+            </div>` // html body
+    };
+    console.log(req.body);
+    res.json({ status: 'Request Successful!' });
+    // send mail with defined transport object
+    transporter.sendMail(projectMailOptions, function (error, info) {
+        if (error) {
+            res.json({ status: 'Request Failed' });
+            console.log(error);
+        }
+        else {
+            console.log('Message sent: ' + info.response);
+            res.json({ status: "Email sent" });
+        }
+    });
+});
+app.post("/contact", (req, res) => {
+    const mailOptions = {
+        from: process.env.A1,
+        to: process.env.A4,
+        subject: `${req.body.contactUsName} sent you a message from the Indige Contact Form!`,
+        text: `${req.body.message}`,
+        html: `<div style="margin: 20px; text-align: left; border: solid 1px grey; border-radius: 5px; padding: 20px;">
+              <h3 style="text-align: center;">You recieved a new message from your Indige Form </h3>
+              <hr style="margin: 20px; color: grey;"/>
+              <br/>
+              <h3>Name:</h3>
+              <p>${req.body.contactUsName}</p>
+              <h3>Email:</h3>
+              <p>${req.body.contactUsEmail}</p>
+              <h3>Subject:</h3>
+              <p>${req.body.subject}</p>
+              <h3>Message below:</h3>
+              <p>${req.body.message}</p>
+            </div>` // html body
+    };
+    console.log(req.body);
+    res.json({ status: 'Request Successful!' });
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            res.json({ status: 'Request Failed' });
+            console.log(error);
+        }
+        else {
+            console.log('Message sent: ' + info.response);
+            res.json({ status: "Email sent" });
+        }
+    });
+});
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+});
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,104 @@
+require('dotenv').config()
+
+import express from 'express';
+const app = express();
+app.use(express.json());
+
+const cors = require('cors');
+app.use(cors())
+
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport({
+    pool: true,
+    host: "uk53.siteground.eu", // hostname
+    secureConnection: false, // TLS requires secureConnection to be false
+    port: 465, // port for secure SMTP
+    tls: {
+      ciphers:'SSLv3'
+    },
+    auth: {
+      user: process.env.A1,
+      pass: process.env.A2
+    }
+})
+
+transporter.verify((error) => {
+  if (error) {console.log(error);}
+  else {console.log("Ready to Send");}
+});
+
+app.post("/submit-project", (req,res)=>{
+    const projectMailOptions = {
+      from: process.env.A1, // sender address (who sends)
+      to: process.env.A4, // list of receivers (who receives)
+      subject: `${req.body.name} has sent you a project idea called ${req.body.projectName}!`, // Subject line
+      text: `${req.body.description}`, // plaintext body.contact
+      html: `<div style="margin: 20px; text-align: left; border: solid 1px grey; border-radius: 5px; padding: 20px;">
+              <h3 style="text-align: center;">You recieved a new message from your Indige Form </h3>
+              <hr style="margin: 20px; color: grey;"/>
+              <br/>
+              <h3>Name:</h3>
+              <p>${req.body.name}</p>
+              <h3>Email:</h3>
+              <p>${req.body.email}</p>
+              <h3>Project Name:</h3>
+              <p>${req.body.projectName}</p>
+              <h3>Project Description:</h3>
+              <p>${req.body.description}</p>
+            </div>` // html body
+    };
+      console.log(req.body)
+      res.json({status: 'Request Successful!'})
+
+    // send mail with defined transport object
+    transporter.sendMail(projectMailOptions, function(error, info){
+        if(error){
+          res.json({status: 'Request Failed'})
+          console.log(error);
+        } else {
+          console.log('Message sent: ' + info.response);
+          res.json({ status: "Email sent" });
+        }
+    });
+  })
+
+  app.post("/contact", (req,res)=>{
+    const mailOptions = {
+      from: process.env.A1, // sender address (who sends)
+      to: process.env.A4, // list of receivers (who receives)
+      subject: `${req.body.contactUsName} sent you a message from the Indige Contact Form!`, // Subject line
+      text: `${req.body.message}`, // plaintext body.contact
+      html: `<div style="margin: 20px; text-align: left; border: solid 1px grey; border-radius: 5px; padding: 20px;">
+              <h3 style="text-align: center;">You recieved a new message from your Indige Form </h3>
+              <hr style="margin: 20px; color: grey;"/>
+              <br/>
+              <h3>Name:</h3>
+              <p>${req.body.contactUsName}</p>
+              <h3>Email:</h3>
+              <p>${req.body.contactUsEmail}</p>
+              <h3>Subject:</h3>
+              <p>${req.body.subject}</p>
+              <h3>Message below:</h3>
+              <p>${req.body.message}</p>
+            </div>` // html body
+    };
+      console.log(req.body)
+      res.json({status: 'Request Successful!'})
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+          res.json({status: 'Request Failed'})
+          console.log(error);
+        } else {
+          console.log('Message sent: ' + info.response);
+          res.json({ status: "Email sent" });
+        }
+    });
+  })
+
+const port = process.env.PORT || 5000
+
+app.listen(port,() =>{
+console.log(`App running on port ${port}`)
+})
